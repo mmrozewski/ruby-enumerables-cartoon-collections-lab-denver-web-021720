1411b6bf85d0259c1df88c2e8090d33d337ca1a4
Mateusz Mrozewski <mmrozews@gmail.com>
2020-02-11 00:35:20 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,7 @@ def roll_call_dwarves(dwarf_array)
 end
 
 def summon_captain_planet(planeteer_calls)
-  # Your code here
+  lunch_menu.map{|item| "#{item}!" }
 end
 
 def long_planeteer_calls# code an argument here
