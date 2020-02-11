58c9939d168ea025666d04fb5cafef0ca0fbb84f
Mateusz Mrozewski <mmrozews@gmail.com>
2020-02-11 01:50:20 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,6 +18,6 @@ end
 def find_the_cheese(array)
   i = 0
   cheese_types = ["cheddar", "gouda", "camembert"]
-  commonality = array & cheese_types
+  commonality = (array & cheese_types).join
 
 end
