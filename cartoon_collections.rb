c5acc0aad06513f7d0ea3047e48a8cb446b1a3b7
Mateusz Mrozewski <mmrozews@gmail.com>
2020-02-11 01:20:55 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,8 +9,8 @@ def summon_captain_planet(planeteer_calls)
   planeteer_calls.map{|item| "#{item.capitalize}!" }
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  
 end
 
 def find_the_cheese# code an argument here
