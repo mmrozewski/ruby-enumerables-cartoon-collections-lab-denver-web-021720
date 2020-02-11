63945bca32f59b69bbc452204ebc8f31e0038988
Mateusz Mrozewski <mmrozews@gmail.com>
2020-02-11 01:00:30 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,8 +1,9 @@
 def roll_call_dwarves(dwarf_array)
   array = []
   dwarf_array.each_with_index do |value, index|
-    puts "#{index}: #{value}"
+    array << "#{index}: #{value}"
   end
+    puts array[index]
 end
 
 def summon_captain_planet(planeteer_calls)
